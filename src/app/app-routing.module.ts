4704bfd2343c8072abadae61ede797883f61078e
Vinicius Pires <2viniciuscpires@gmail.com>
2020-09-18 22:32:23 -0300

    Added components for routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,32 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './views/home/home.component'
+import { ForgotComponent } from './views/forgot/forgot.component'
+import { ProfileComponent } from './views/profile/profile.component'
+import { SignupComponent } from './views/signup/signup.component'
+import { StudiesComponent } from './views/studies/studies.component'
+import { TeacherFormComponent } from './views/teacher-form/teacher-form.component'
+
+const routes: Routes = [{
+  path: '',
+  component: HomeComponent
+}, {
+  path: '/signup',
+  component: SignupComponent
+}, {
+  path: '/forgot',
+  component: ForgotComponent
+}, {
+  path: '/sudies',
+  component: StudiesComponent
+}, {
+  path: '/profile',
+  component: ProfileComponent
+}, {
+  path: '/teacherform',
+  component: TeacherFormComponent
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,23 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './views/home/home.component';
+import { StudiesComponent } from './views/studies/studies.component';
+import { ProfileComponent } from './views/profile/profile.component';
+import { TeacherFormComponent } from './views/teacher-form/teacher-form.component';
+import { SignupComponent } from './views/signup/signup.component';
+import { ForgotComponent } from './views/forgot/forgot.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    HomeComponent,
+    StudiesComponent,
+    ProfileComponent,
+    TeacherFormComponent,
+    SignupComponent,
+    ForgotComponent
   ],
   imports: [
     BrowserModule,
