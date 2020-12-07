4635e22f4d87af12645cc74ba76cbe55bc32d339
Vinicius Pires <2viniciuscpires@gmail.com>
2020-12-06 22:38:50 -0300

    Fix router

--- src/app/views/profile/profile.module.ts
+++ src/app/views/profile/profile.module.ts
@@ -3,6 +3,8 @@ import { CommonModule } from '@angular/common';
 import { ProfileViewComponent } from './profile-view/profile-view.component';
 import { ProfileEditComponent } from './profile-edit/profile-edit.component';
 import { ProfileComponent } from './profile.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
@@ -11,7 +13,9 @@ import { ProfileComponent } from './profile.component';
     ProfileEditComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    BrowserModule,
+    RouterModule
   ]
 })
 export class ProfileModule { }
